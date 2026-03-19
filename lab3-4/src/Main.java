72f70a020ce83fa3430d99d8287ae710849f7c8b
Muhammad <beautifulnoise03@gmail.com//>
2026-03-19 21:35:48 +0500

    updated filee

---
+++ lab3-4/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Git Lab!");
+
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        int div = a / b;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + diff);
+        System.out.println("Multiplication: " + mul);
+        System.out.println("Division: " + div);
+    }
+}
